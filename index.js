cdff2c9a5183df15d81ec78540ae585f1aee12f5
Rocky Dey <coderrocky25@gmail.com>
2024-04-23 20:42:41 +0600

    add coffee post api done

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffees");
+
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
