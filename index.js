21e880a87fdfe3388869179bdc1499e4a2772716
Rocky Dey <coderrocky25@gmail.com>
2024-04-24 13:36:07 +0600

    get api via id for view coffee complete

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/coffees", async (req, res) => {
       const newCoffee = req.body;
       const result = await coffeeCollection.insertOne(newCoffee);
